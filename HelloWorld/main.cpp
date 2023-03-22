f01546d92f44c5fd7985bd465d463c45c5e98bb3
BrockBrierley <brock_brierley@outlook.com>
2023-03-22 12:48:32 +1100

    Added in a repeating statement

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -3,4 +3,11 @@
 int main()
 {
 	std::cout << "Hello World, This is the beginning of my Version Control Assignment" << std::endl;
+
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << "I have repeated myself " << i << " times." << std::endl;
+	}
 }
