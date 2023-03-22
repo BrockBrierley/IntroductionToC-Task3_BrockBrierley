8df517270f7b3630cbe49f0eca4c5669eb4cc8ca
BrockBrierley <brock_brierley@outlook.com>
2023-03-22 12:55:23 +1100

    attempting to create a merge conflict

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -5,4 +5,8 @@ int main()
 	std::cout << "Hello World"<< std::endl;
 	std::cout << "On Second thoughts it is easier to read and understand the code," << std::endl;
 	std::cout << "if it is written like this" << std::endl;
+
+
+	//I am attempting to create a merge conflict
+	//This should not make the system happy
 }
